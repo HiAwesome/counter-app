7cda0e36a4b329911400038460ad792d5fa0862d
moqi <moqimoqidea@gmail.com>
2021-03-21 08:44:40 +0800

    update app.py add page show words

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return "What's up Docker Deep Divers! You've visited me {} times.\n".format(count)
+    return "Moqi test!!! What's up Docker Deep Divers! You've visited me {} times.\n".format(count)
 
 
 if __name__ == "__main__":
